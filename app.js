d39c15f42d315260cf306d23cd225db9f6de0a9d
dwharper77 <dwharper77@gmail.com>
2026-01-19 11:03:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Register service worker
-console.log('DEV v1 loaded');
+console.log('Stage Viewer v2.0 loaded');
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('service-worker.js');
